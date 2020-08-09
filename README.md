fe3cb20f9c3d83acacb11ac6b8b32ebd188240ce
Joshua Bowen <joshnbowen@gmail.com>
2020-08-09 14:01:48 -0400

    Add client

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+Replace config.get('mongoURI') with your own Mongo URI 
+        config.get('jwtSecret') with your JWT Secret
 
--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Started on port ${PORT}`));
 
